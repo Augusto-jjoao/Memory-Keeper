2b1852c649a2777e82e8d8d24dc71f7117ae2980
Joao Augusto <joao.rodrigues.cruz.jar@gmail.com>
2025-10-07 10:55:55 -0300

    Automatizar Allowed_Hosts pro Render

--- backend/settings.py
+++ backend/settings.py
@@ -28,7 +28,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='sua-secret-key-local')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1').split(' ')
+ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
